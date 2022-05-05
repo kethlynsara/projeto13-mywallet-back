d65766686c3a4bdad75bf7b52623fdd0a3c1263f
Kethlyn <kethlynsaraa@gmail.com>
2022-05-05 14:15:27 -0300

    POST /registros: validação e persistência dos dados de registro

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { MongoClient, ObjectId } from "mongodb";
-import express from "express";
+import express, { response } from "express";
 //import cors from "cors";
 import Joi from "joi";
+import dayjs from "dayjs";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
@@ -26,40 +27,76 @@ app.post("/sign-up", async (req, res) => {
   const body = req.body;
 
   const schema = Joi.object({
-    name: Joi.string().required(),
+    nome: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().pattern(/^[a-zA-Z]{3}[0-9]{2}$/).required()
+    senha: Joi.string().pattern(/^[a-zA-Z]{3}[0-9]{2}$/).required()
   })
 
   const validation = schema.validate(body);
 
   try {
-    const passwordHash = bcrypt.hashSync(body.password, 10);
-    await db.collection("signup").insertOne({...body, password: passwordHash});
+    const senhaHash = bcrypt.hashSync(body.senha, 10);
+    await db.collection("usuarios").insertOne({...body, senha: senhaHash});
     res.sendStatus(201);
   } catch(e) {
-    res.sendStatus(422); 
+    res.status(500).send("Não foi possível cadastrar usuário"); 
   }
  
 });
 
 app.post("/sign-in", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, senha } = req.body;
 
   try {
-    const user = await db.collection("signup").findOne({email});
-    if (user && bcrypt.compareSync(password, user.password)) {
+    const user = await db.collection("usuarios").findOne({email});
+    if (user && bcrypt.compareSync(senha, user.senha)) {
       const token = uuid();
 
-      await db.collection("sessions").insertOne({userId: user._id, token});
+      await db.collection("sessoes").insertOne({userId: user._id, token});
 
       return res.sendStatus(201);
     } else {
-      return res.sendStatus(422); //user não encontrado
+      return res.status(401).send("Usuário não encontrado"); //user não encontrado
     }
   }catch(e) {
-    res.sendStatus(500);
+    res.status(500).send("Não foi possível fazer login do usuário"); 
   }
 });
+
+app.post("/registros", async (req, res) => {
+  const registro = req.body;
+
+  const registroSchema = Joi.object({
+    valor: Joi.number().required(),
+    descricao: Joi.string().required(),
+    tipo: Joi.string().required(),
+    token: Joi.string().required()
+  });
+
+  const validacao = registroSchema.validate(registro);
+
+  if (validacao.error) {
+    return res.status(422).send("Confira seus dados!");
+  }
+
+  try {
+    const { valor, descricao, tipo, token } = registro;
+    
+    const sessao = await db.collection("sessoes").findOne({token});
+
+    if (!sessao) {
+      res.status(401).send("Usuário não encontrado");
+    }
+
+    await db.collection("registros").insertOne({valor, descricao, tipo, userId: sessao._id, data: dayjs().format('DD/MM')})
+
+    res.sendStatus(201);
+  }catch(e) {
+    res.status(500).send("Não foi possível fazer o registro");
+  }
+
+})
+
+
 
 app.listen(5000);
